326213d0a7035610217a0d2373ee492c859104f3
Rakesh Avasarala <rakesh.avasarala@digital.hmrc.gov.uk>
2016-08-23 16:34:05 +0100

    Fix compilation issue

--- test/uk/gov/hmrc/selfassessmentapi/domain/LiabilitySpec.scala
+++ test/uk/gov/hmrc/selfassessmentapi/domain/LiabilitySpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.selfassessmentapi.domain
 
 import org.json.{JSONArray, JSONObject}
@@ -59,7 +75,7 @@ class LiabilitySpec extends JsonSpec {
         interestFromUk = 500,
         totalDeductionFromUkProperties =
           500,
-        deductionFromUkProperties = Seq(TaxPaidForUkProperty(sourceId = "abc", amount = 500)),
+        deductionFromUkProperties = Seq(TaxPaidForUkProperty(sourceId = "abc", taxPaid = 500)),
         ukTaxPaid = 500,
         ukTaxesPaidForEmployments = Seq(MongoUkTaxPaidForEmployment("abc", 500))))
     }
