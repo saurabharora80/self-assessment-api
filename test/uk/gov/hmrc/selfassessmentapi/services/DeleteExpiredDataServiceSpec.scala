7d20685d3abadea5d0151b0a4c401ddbb7ecb4aa
Jacob Taylor Hindle <jacob.taylor-hindle@digital.hmrc.gov.uk>
2016-09-30 16:38:46 +0100

    [JTH] Fix issue where data was not being associated with a Self-Assessment in the DeleteExpiredDataServiceSpec.

--- test/uk/gov/hmrc/selfassessmentapi/services/DeleteExpiredDataServiceSpec.scala
+++ test/uk/gov/hmrc/selfassessmentapi/services/DeleteExpiredDataServiceSpec.scala
@@ -35,7 +35,6 @@ import uk.gov.hmrc.selfassessmentapi.repositories.{JobHistoryMongoRepository, Se
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-
 class DeleteExpiredDataServiceSpec extends MongoEmbeddedDatabase with MockitoSugar with ScalaFutures {
 
   private val saRepo = new SelfAssessmentMongoRepository
@@ -46,86 +45,111 @@ class DeleteExpiredDataServiceSpec extends MongoEmbeddedDatabase with MockitoSug
   private val ukPropertyRepo = new UKPropertiesMongoRepository
   private val jobRepo = new JobHistoryMongoRepository
 
-  val saUtr = generateSaUtr()
-  val lastModifiedDate = DateTime.now().minusWeeks(1)
+  private val saUtr = generateSaUtr()
+  private val saUtr2 = generateSaUtr()
+  private val saUtr3 = generateSaUtr()
+  private val lastModifiedDate = DateTime.now().minusWeeks(1)
 
   implicit override val patienceConfig = PatienceConfig(timeout = Span(15, Seconds), interval = Span(300, Millis))
 
+  /*
+   * Inserts a self-assessment in to the database and verifies that it is removed correctly
+   * along with any additional data that was inserted in `block`.
+   */
+  private def withInsertSelfAssessment(block: () => Unit): Unit = {
+    val sa1 = SelfAssessment(BSONObjectID.generate, saUtr, taxYear, DateTime.now().minusMonths(1), DateTime.now().minusMonths(1))
+    val sa2 = SelfAssessment(BSONObjectID.generate, saUtr2, taxYear, DateTime.now().minusMonths(2), DateTime.now().minusMonths(2))
+    val latestSa3 = SelfAssessment(BSONObjectID.generate, saUtr3, taxYear, DateTime.now().minusDays(1), DateTime.now().minusDays(1))
+
+    insertSelfAssessmentRecords(sa1, sa2, latestSa3)
+
+    val service = new DeleteExpiredDataService(saRepo, seRepo, uiRepo, empRepo, fhlRepo, ukPropertyRepo, jobRepo)
+
+    whenReady(service.deleteExpiredData(lastModifiedDate)) { _ =>
+      val saRecords = saRepo.findAll()
+
+      whenReady(saRecords) { _ =>
+        saRecords.size shouldBe 1
+        saRecords.head.saUtr == latestSa3.saUtr && saRecords.head.taxYear == latestSa3.taxYear shouldBe true
+
+        block()
+      }
+    }
+  }
+
   "deleteExpiredData" should {
 
-    "delete only the expired records (older than the lastModifiedDate) and not the latest records which have not expired" in {
-      val lastModifiedDate = DateTime.now.minusWeeks(1)
-      val saUtr2 = generateSaUtr()
-      val saUtr3 = generateSaUtr()
-
-      val sa1 = SelfAssessment(BSONObjectID.generate, saUtr, taxYear, DateTime.now().minusMonths(1), DateTime.now().minusMonths(1))
-      val sa2 = SelfAssessment(BSONObjectID.generate, saUtr2, taxYear, DateTime.now().minusMonths(2), DateTime.now().minusMonths(2))
-      val latestSa3 = SelfAssessment(BSONObjectID.generate, saUtr3, taxYear, DateTime.now().minusDays(1), DateTime.now().minusDays(1))
-
+    "delete only the expired records (older than the lastModifiedDate) and not the latest records for self-employments" in {
       val se1 = SelfEmployment.create(saUtr, taxYear, selfemployment.SelfEmployment.example())
       val se2 = SelfEmployment.create(saUtr2, taxYear, selfemployment.SelfEmployment.example())
       val latestSe3 = SelfEmployment.create(saUtr3, taxYear, selfemployment.SelfEmployment.example())
 
-      val emp1 = Employment.create(saUtr, taxYear, employment.Employment.example())
-      val emp2 = Employment.create(saUtr2, taxYear, employment.Employment.example())
-      val latestEmp3 = Employment.create(saUtr3, taxYear, employment.Employment.example())
-
-      val fhl1 = FurnishedHolidayLettings.create(saUtr, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
-      val fhl2 = FurnishedHolidayLettings.create(saUtr2, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
-      val latestFhl3 = FurnishedHolidayLettings.create(saUtr3, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
-
-      val property1 = UKProperties.create(saUtr, taxYear, ukproperty.UKProperty.example())
-      val property2 = UKProperties.create(saUtr2, taxYear, ukproperty.UKProperty.example())
-      val latestProperty3 = UKProperties.create(saUtr3, taxYear, ukproperty.UKProperty.example())
-
-
-      insertSelfAssessmentRecords(sa1, sa2, latestSa3)
       insertSelfEmploymentRecords(se1, se2, latestSe3)
-      insertEmploymentRecords(emp1, emp2, latestEmp3)
-      insertFHLRecords(fhl1, fhl2, latestFhl3)
-      insertUKPropertyRecords(property1, property2, latestProperty3)
 
-      val service = new DeleteExpiredDataService(saRepo, seRepo, uiRepo, empRepo, fhlRepo, ukPropertyRepo, jobRepo)
+      withInsertSelfAssessment { () =>
+        val seRecords = seRepo.findAll()
 
-      val nrDeleted = service.deleteExpiredData(lastModifiedDate)
-
-      whenReady(nrDeleted) { _ =>
-
-        val saRecords = saRepo.findAll()
-
-        whenReady(saRecords) { _ =>
-          val seRecords = seRepo.findAll()
-          val empRecords = empRepo.findAll()
-          val fhlRecords = fhlRepo.findAll()
-          val propertyRecords = ukPropertyRepo.findAll()
-
-          whenReady(seRecords) { _ =>
-
-            saRecords.size shouldBe 1
-            saRecords.head.saUtr == latestSa3.saUtr && saRecords.head.taxYear == latestSa3.taxYear shouldBe true
-
-            seRecords.size shouldBe 1
-            seRecords.head.saUtr == latestSe3.saUtr && seRecords.head.taxYear == latestSe3.taxYear shouldBe true
-          }
-
-          whenReady(empRecords) { _ =>
-            empRecords.size shouldBe 1
-            empRecords.head.saUtr == latestEmp3.saUtr && empRecords.head.taxYear == latestEmp3.taxYear shouldBe true
-          }
-
-          whenReady(fhlRecords) { _ =>
-            fhlRecords.size shouldBe 1
-            fhlRecords.head.saUtr == latestFhl3.saUtr && fhlRecords.head.taxYear == latestFhl3.taxYear shouldBe true
-          }
-
-          whenReady(propertyRecords) { _ =>
-            propertyRecords.size shouldBe 1
-            propertyRecords.head.saUtr == latestProperty3.saUtr && propertyRecords.head.taxYear == latestProperty3.taxYear shouldBe true
-          }
+        whenReady(seRecords) { _ =>
+          seRecords.size shouldBe 1
+          seRecords.head.saUtr shouldBe latestSe3.saUtr
+          seRecords.head.taxYear shouldBe latestSe3.taxYear
         }
       }
     }
 
+    "delete only the expired records (older than the lastModifiedDate) and not the latest records for employments" in {
+      val emp1 = Employment.create(saUtr, taxYear, employment.Employment.example())
+      val emp2 = Employment.create(saUtr2, taxYear, employment.Employment.example())
+      val latestEmp3 = Employment.create(saUtr3, taxYear, employment.Employment.example())
+
+      insertEmploymentRecords(emp1, emp2, latestEmp3)
+
+      withInsertSelfAssessment { () =>
+        val empRecords = empRepo.findAll()
+
+        whenReady(empRecords) { _ =>
+          empRecords.size shouldBe 1
+          empRecords.head.saUtr shouldBe latestEmp3.saUtr
+          empRecords.head.taxYear shouldBe latestEmp3.taxYear
+        }
+      }
+    }
+
+    "delete only the expired records (older than the lastModifiedDate) and not the latest records for FHLs" in {
+      val fhl1 = FurnishedHolidayLettings.create(saUtr, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
+      val fhl2 = FurnishedHolidayLettings.create(saUtr2, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
+      val latestFhl3 = FurnishedHolidayLettings.create(saUtr3, taxYear, furnishedholidaylettings.FurnishedHolidayLetting.example())
+
+      insertFHLRecords(fhl1, fhl2, latestFhl3)
+
+      withInsertSelfAssessment { () =>
+        val fhlRecords = fhlRepo.findAll()
+
+        whenReady(fhlRecords) { _ =>
+          fhlRecords.size shouldBe 1
+          fhlRecords.head.saUtr shouldBe latestFhl3.saUtr
+          fhlRecords.head.taxYear shouldBe latestFhl3.taxYear
+        }
+      }
+    }
+
+    "delete only the expired records (older than the lastModifiedDate) and not the latest records for UK properties" in {
+      val property1 = UKProperties.create(saUtr, taxYear, ukproperty.UKProperty.example())
+      val property2 = UKProperties.create(saUtr2, taxYear, ukproperty.UKProperty.example())
+      val latestProperty3 = UKProperties.create(saUtr3, taxYear, ukproperty.UKProperty.example())
+
+      insertUKPropertyRecords(property1, property2, latestProperty3)
+
+      withInsertSelfAssessment { () =>
+        val propertyRecords = ukPropertyRepo.findAll()
+
+        whenReady(propertyRecords) { _ =>
+          propertyRecords.size shouldBe 1
+          propertyRecords.head.saUtr shouldBe latestProperty3.saUtr
+          propertyRecords.head.taxYear shouldBe latestProperty3.taxYear
+        }
+      }
+    }
 
     "mark job as failed if there is an exception when trying to delete records from self assessment" in {
       val sa1 = SelfAssessment(BSONObjectID.generate, saUtr, taxYear, DateTime.now().minusMonths(1), DateTime.now().minusMonths(1))
