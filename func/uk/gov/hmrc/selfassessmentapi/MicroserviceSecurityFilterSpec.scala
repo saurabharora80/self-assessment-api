23f2375380db086e8042c06cdfe93366c14ffdf1
Dario Abdulrehman <dario.rehman@gmail.com>
2017-02-03 17:19:57 +0000

    [MTDSA-844][CK|DA] Add test for additional endpoint

--- func/uk/gov/hmrc/selfassessmentapi/MicroserviceSecurityFilterSpec.scala
+++ func/uk/gov/hmrc/selfassessmentapi/MicroserviceSecurityFilterSpec.scala
@@ -4,18 +4,19 @@ import uk.gov.hmrc.support.BaseFunctionalSpec
 
 class MicroserviceSecurityFilterSpec extends BaseFunctionalSpec {
 
-  "Request with X-Test-Scenario = AGENT_NOT_SUBSCRIBED" should {
+  "Request for self-employments with X-Test-Scenario = AGENT_NOT_SUBSCRIBED" should {
     "return HTTP 403 with error code informing Agent should be subscribed to Agent Services" in {
       given()
         .userIsAuthorisedForTheResource(nino)
         .when()
-        .get(s"/ni/$nino/self-employments").withHeaders("X-Test-Scenario", "AGENT_NOT_SUBSCRIBED")
+        .get(s"/ni/$nino/self-employments")
+        .withHeaders("X-Test-Scenario", "AGENT_NOT_SUBSCRIBED")
         .thenAssertThat()
         .statusIs(403)
     }
   }
 
-  "Request with no X-Test-Scenario" should {
+  "Request for self-employments with no X-Test-Scenario" should {
     "return HTTP 200" in {
       given()
         .userIsAuthorisedForTheResource(nino)
@@ -26,14 +27,51 @@ class MicroserviceSecurityFilterSpec extends BaseFunctionalSpec {
     }
   }
 
-  "Request with invalid X-Test-Scenario" should {
+  "Request for self-employments with invalid X-Test-Scenario" should {
     "return HTTP 200" in {
       given()
         .userIsAuthorisedForTheResource(nino)
         .when()
-        .get(s"/ni/$nino/self-employments").withHeaders("X-Test-Scenario", "FOO")
+        .get(s"/ni/$nino/self-employments")
+        .withHeaders("X-Test-Scenario", "FOO")
         .thenAssertThat()
         .statusIs(200)
     }
   }
+
+  "Request for dividends with X-Test-Scenario = AGENT_NOT_SUBSCRIBED" should {
+    "return HTTP 403 with error code informing Agent should be subscribed to Agent Services" in {
+      given()
+        .userIsAuthorisedForTheResource(nino)
+        .when()
+        .get(s"/ni/$nino/dividends/$taxYear")
+        .withHeaders("X-Test-Scenario", "AGENT_NOT_SUBSCRIBED")
+        .thenAssertThat()
+        .statusIs(403)
+    }
+  }
+
+  "Request for dividends with no X-Test-Scenario" should {
+    "return HTTP 200" in {
+      given()
+        .userIsAuthorisedForTheResource(nino)
+        .when()
+        .get(s"/ni/$nino/dividends/$taxYear")
+        .thenAssertThat()
+        .statusIs(200)
+    }
+  }
+
+  "Request for dividends with invalid X-Test-Scenario" should {
+    "return HTTP 200" in {
+      given()
+        .userIsAuthorisedForTheResource(nino)
+        .when()
+        .get(s"/ni/$nino/dividends/$taxYear")
+        .withHeaders("X-Test-Scenario", "FOO")
+        .thenAssertThat()
+        .statusIs(200)
+    }
+  }
+
 }
