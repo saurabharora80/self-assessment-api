04880788c28a910f29a2b2ee86446d6884a388b4
Dario Abdulrehman <dario.rehman@gmail.com>
2017-02-09 14:35:43 +0000

    [MTDSA-847][CK|DA] Fix audt configuration

--- app/uk/gov/hmrc/selfassessmentapi/config/AppContext.scala
+++ app/uk/gov/hmrc/selfassessmentapi/config/AppContext.scala
@@ -33,7 +33,7 @@ object AppContext extends ServicesConfig {
   lazy val featureSwitch = current.configuration.getConfig(s"$env.feature-switch")
   lazy val updateTaxYearPropertiesEnabled: Boolean = current.configuration.getBoolean(s"update-tax-year-properties.enabled").getOrElse(true)
   lazy val authEnabled: Boolean = current.configuration.getBoolean(s"auth.enabled").getOrElse(true)
-  lazy val auditEnabled: Boolean = current.configuration.getBoolean(s"audit.enabled").getOrElse(true)
+  lazy val auditEnabled: Boolean = current.configuration.getBoolean(s"auditing.enabled").getOrElse(true)
   def deleteExpiredDataJob = current.configuration.getConfig(s"$env.scheduling.deleteExpiredDataJob").getOrElse(throw new RuntimeException(s"$env.scheduling.deleteExpiredDataJob is not configured"))
 
   val supportedTaxYears: Set[String] = Set("2016-17")
