b29189db9fd85f84f1d6010d65dc6acbae690680
Jacob Taylor Hindle <jacob.taylor-hindle@digital.hmrc.gov.uk>
2017-03-28 11:24:21 +0100

    Add auditing for DES requests.

--- app/uk/gov/hmrc/selfassessmentapi/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/selfassessmentapi/config/microserviceWiring.scala
@@ -17,6 +17,7 @@
 package uk.gov.hmrc.selfassessmentapi.config
 
 
+import uk.gov.hmrc.play.audit.http.HttpAuditing
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
@@ -24,8 +25,9 @@ import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
 
-object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
-  override val hooks: Seq[HttpHook] = NoneRequired
+object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName with RunMode with HttpAuditing {
+  override val hooks: Seq[HttpHook] = Seq(AuditingHook)
+  override def auditConnector: AuditConnector = MicroserviceAuditConnector
 }
 
 object MicroserviceAuditConnector extends AuditConnector with RunMode {
